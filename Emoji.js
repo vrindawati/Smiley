eaca30819925d9f3da42f9f710d6dde09a2b0c57
vrindawati <vrindawatiyadav23@navgurukul.org>
2024-08-03 17:17:34 +0530

    Add files via upload

---
+++ Emoji.js
@@ -0,0 +1,14 @@
+
+function emoji(){
+    let sad = document.getElementById("SG");
+    let happy = document.getElementById("Button");
+    happy.textContent = "Happy";
+    sad.src ="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/65532/happy-emoji-clipart-md.png";
+    sad.style.height = "500px";
+}
+
+
+
+
+
+
